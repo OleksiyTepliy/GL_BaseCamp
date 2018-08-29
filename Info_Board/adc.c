b650f003e9d67f505b8e319df89759f8eca7ddf8
OleksiyTepliy <emblink182@gmail.com>
2018-08-29 17:37:32 +0300

    Info_Board

---
+++ Info_Board/adc.c
@@ -0,0 +1,28 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
+#include "Board_Info.h"
+
+extern volatile uint8_t temp;
+extern volatile bool flags[];
+
+void adc_init()
+{
+	ADMUX |= (1 << REFS0); //AV CC with external capacitor at AREF pin
+	//ADLAR right adjustment
+	//ADMUX 0000 ADC0 configured
+	//ADMUX |= 1 << MUX3 for temp sensor
+	ADCSRA |= (1 << ADEN) | (1<<ADLAR) | (1 << ADIE) | (1<<ADSC); // ADC Interrupt Enable
+	ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2); // ADC Prescaler Select to 128
+	//ADCSRB = 0; // free mode 
+}
+	// ADCSRA |=(1<<ADEN)|(1<<ADFR)|(1<<ADIE)|(1<<ADSC)|(3<<ADPS0); // set 0xFF
+	// ADMUX  |=(1<<REFS0)|(1<<ADLAR);
+
+ISR(ADC_vect, ISR_BLOCK)
+{
+	temp = ADCH;
+	flags[U_PHOTO] = true;
+}
+//ADCSRA |= 1 << ADSC start conversion
+
